36391092e512a2999beed283733793c574890d19
alizadake <kanan.alizada@kapitalbank.az>
2023-01-14 11:58:01 +0400

    producer and consumer setup

---
+++ csv-consumer/src/main/java/com/memphis/csv/consumer/CsvConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.memphis.csv.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvConsumerApplication.class, args);
+	}
+
+}
---
+++ csv-producer/src/main/java/com/memphis/csv/producer/CsvProducerApplication.java
@@ -0,0 +1,13 @@
+package com.memphis.csv.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvProducerApplication.class, args);
+	}
+
+}
